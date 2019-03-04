7bdad1b12dbbfdf9e0396904668d6d4e616d4e81
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-03-04 10:27:07 -0800

    Wrote initialize method to pass filled in tests

--- lib/card.rb
+++ lib/card.rb
@@ -5,10 +5,15 @@ class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
+    unless value > 0 && value < 14
+      raise ArgumentError, "Value must be between 1 and 13."
+    end
     @value = value
+
+    unless [:hearts, :spades, :clubs, :diamonds].include?(suit)
+      raise ArgumentError, "Invalid suit, please input a valid suit."
+    end
     @suit = suit
-
-
   end
 
   def to_s
