107bfec4a998854ae543f1144540018bd414dbfe
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-03-04 11:24:09 -0800

    Wrote instantiation test for deck.rb and method to pass

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,17 @@
-
-# deck.rb
-
 require_relative 'card'
 
 class Deck
+  attr_reader :cards
 
   def initialize
-
+    cards = []
+    [:hearts, :spades, :clubs, :diamonds].each do |suit|
+      (1..13).each do |value|
+        card = Card.new(value, suit)
+        cards << card
+      end
+    end
+    @cards = cards
   end
 
   def draw
--- specs/deck_spec.rb
+++ specs/deck_spec.rb
@@ -1,11 +1,16 @@
 
 
-require_relative 'spec_helper'
+require_relative "spec_helper"
 
 describe Deck do
   # Write tests for the Deck class.
   # What should you test?
-    # 1.  Expected Behavior
-    # 2.  Edge cases
+  # 1.  Expected Behavior
+  # 2.  Edge cases
+  it "creates a deck with 52 cards" do
+    new_deck = Deck.new
 
+    expect(new_deck).must_be_kind_of Deck
+    expect(new_deck.cards.length).must_equal 52
+  end
 end
